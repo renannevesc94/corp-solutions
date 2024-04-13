aea5a7040536e54870f13cb6e0932bbb71aac068
renannevesc94 <renan.epd@gmail.com>
2024-04-13 01:00:44 -0300

    created component Button with variants primary and secondary using clsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,21 @@
-export const Button = () => {
-  return <button></button>;
+import React from "react";
+import styles from "./style.module.css";
+import clsx from "clsx";
+
+type ButtonProps = {
+  variant: "primary" | "secondary";
+} & React.ComponentPropsWithoutRef<"button">;
+
+export const Button = ({ children, variant, ...props }: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={clsx(styles.button, {
+        [styles.primary]: variant === "primary",
+        [styles.secondary]: variant === "secondary",
+      })}
+    >
+      {children}
+    </button>
+  );
 };
--- src/modules/auth/components/FormLogin/index.tsx
+++ src/modules/auth/components/FormLogin/index.tsx
@@ -1,13 +1,15 @@
 import { Input } from "../../../../components/Input";
-import style from "./style.module.css";
+import styles from "./style.module.css";
 import { InputPassword } from "../../../../components/InputPassword";
+import { Button } from "../../../../components/Button";
 
 export const FormLogin = () => {
   return (
-    <div className={style.container}>
+    <div className={styles.container}>
       <h2>Login</h2>
       <Input label="Seu e-mail" />
       <InputPassword label="Sua a senha" />
+      <Button variant="secondary">Entrar</Button>
     </div>
   );
 };
