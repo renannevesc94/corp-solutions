f53572eae281fa956bc6d0e1b7a63a0ed99f8ff4
renannevesc94 <renan.epd@gmail.com>
2024-04-16 20:54:29 -0300

    add styles card courses

--- src/modules/Cursos/components/Card/card.tsx
+++ src/modules/Cursos/components/Card/card.tsx
@@ -1,25 +1,33 @@
 /* import Carousel from "../Carousel/carousel"; */
 
+import { EyesIcon, TimerIcon } from "../../../../components/Icons";
 import { Rating } from "../Rating";
 import styles from "./style.module.css";
 
-type CardProps = {
+/* type CardProps = {
   id: string;
   imagem: string;
   title: string;
 };
-
-function Card({ id, imagem, title }: CardProps) {
+ */
+function Card() {
   return (
     <section className={styles.card}>
       <div className={styles.content}>
         <img src="./public/cardsCourse/course01.png" alt="capa do curso" />
-
         <img src="./cardsCourse/logoFull.png" alt="logo corp solution" />
       </div>
 
       <div className={styles.footer}>
-        <Rating />
+        <div>
+          <Rating />
+        </div>
+        <div>
+          <TimerIcon /> <span>5h</span>
+        </div>
+        <div>
+          <EyesIcon /> <span>100</span>
+        </div>
       </div>
     </section>
   );
