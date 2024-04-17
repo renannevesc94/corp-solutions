7c12871d03945be275ba19fa850da4b808b4e5ee
Carolina Dutra <dutracarol001@gmail.com>
2024-04-16 23:48:27 -0300

    ajustes

--- src/modules/Cursos/cursos.tsx
+++ src/modules/Cursos/cursos.tsx
@@ -2,7 +2,8 @@ import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header";
 import Banner from "./components/Banner/banner";
 import Card from "./components/Card/card";
-import styles from "./styles.module.css";
+import Container from "./components/Container/container";
+import "./styles.module.css";
 
 import videos from "./json/db.json";
 import Category from "./components/Category/category";
@@ -27,14 +28,14 @@ export const Cursos = () => {
       <Container>
         {categories.map((categoria, indx) => (
           <Category key={indx} category={categoria}>
-            <Carousel>
+            
               {filterCategory(indx).map(video => (
                 <Card id={video.id} key={video.id} imagem={video.imagem} title={video.title} />
               ))}
-            </Carousel>
           </Category>
         ))}
       </Container>
+      <Footer/>
     </>
   );
 };
